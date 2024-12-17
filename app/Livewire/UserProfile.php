64444ff9945b203fbd7d23f3f4f1ba75ded05f24
Namrata199 <namru.mail@gmail.com>
2024-12-17 11:34:01 +0545

    inital commit

---
+++ app/Livewire/UserProfile.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\User;
+use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Wizard;
+use Filament\Forms\Components\Wizard\Step;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Form;
+use Livewire\Component;
+use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
+
+class UserProfile extends Component implements HasForms
+{
+    use InteractsWithForms;
+
+    public array $data = [];
+
+    public $user;
+
+    public array $debugOptions = [];
+
+    public function mount(): void
+    {
+        $this->form->fill();
+
+        $this->user = User::find(1);
+    }
+
+    public function continueEditing(): void
+    {
+        if ($this->user) {
+            $this->data['data']['media'] = $this->user->getMedia('users')->pluck('uuid', 'uuid')->toArray();
+        }
+    }
+
+    private function saveMediaFiles(User $user): void
+    {
+        if (isset($this->data['data']['media']) && is_array($this->data['data']['media'])) {
+            foreach ($this->data['data']['media'] as $file) {
+                if ($file instanceof TemporaryUploadedFile) {
+                    $user->addMediaFromDisk($file->getRealPath())->toMediaCollection('users');
+                }
+            }
+        }
+    }
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Wizard::make([
+                    Step::make('Image')
+                        ->afterValidation(function () {
+                            $this->saveMediaFiles($this->user);
+                        })
+                        ->schema([
+                            SpatieMediaLibraryFileUpload::make('data.media')
+                                ->multiple()
+                                ->collection('images'),
+                        ]),
+                    Step::make('Info')
+                        ->schema([
+                            TextInput::make('name'),
+                        ]),
+                ]),
+
+            ])->statePath('data');
+    }
+
+    public function save(): void
+    {
+        $this->validate(); // Validate form data
+        $this->user->addMediaFromRequest('data.media') // Example for media upload
+            ->toMediaCollection('images');
+
+        session()->flash('success', 'Profile updated successfully.');
+    }
+
+    public function render()
+    {
+        return view('livewire.user-profile');
+    }
+}
